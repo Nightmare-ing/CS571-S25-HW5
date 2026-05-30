34096b2c110faa7748d7b2d5418473833bdaa367
Qingsong Hu <kingsanhu@gmail.com>
2026-05-30 11:36:39 +0800

    Finish part 4 of HW5
    Last commit has finished part 3.
    By clicking `Show More` button, shows more detailed information about
    buddy, and the button is changed to `Show Less`.
    By clicking `Show Less` button, fold the detailed information

--- src/components/BadgerBudsSummary.jsx
+++ src/components/BadgerBudsSummary.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Button, Card, Image } from "react-bootstrap";
 import BadgerBudsDataContext from "../contexts/BadgerBudsDataContext";
 
 export default function BadgerBudsSummary(props) {
+    const [showMore, setShowMore] = useState(false);
     return (
         <Card>
             <Card.Img
@@ -16,9 +17,21 @@ export default function BadgerBudsSummary(props) {
             />
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
+                {showMore && (
+                    <div>
+                        {props.gender && <Card.Text>{props.gender}</Card.Text>}
+                        {props.breed && <Card.Text>{props.breed}</Card.Text>}
+                        {props.age && <Card.Text>{props.age}</Card.Text>}
+                        {props.description && (
+                            <Card.Text>{props.description}</Card.Text>
+                        )}
+                    </div>
+                )}
             </Card.Body>
             <Card.Footer className="d-flex gap-2">
-                <Button size="sm">Show more</Button>
+                <Button size="sm" onClick={() => setShowMore(!showMore)}>
+                    Show {showMore ? "Less" : "More"}
+                </Button>
                 <Button size="sm" variant="secondary">
                     ❤️ Save
                 </Button>
