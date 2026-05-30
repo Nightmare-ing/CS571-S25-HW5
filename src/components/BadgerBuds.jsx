1f66f5049ebc7a414edf9a2af576777acab110c9
Qingsong Hu <kingsanhu@gmail.com>
2026-05-30 09:40:43 +0800

    Finish part 1 of HW5
    Implement routes, also change API link for public API access

--- src/components/BadgerBuds.jsx
+++ src/components/BadgerBuds.jsx
@@ -5,29 +5,30 @@ import BadgerBudsNavbar from "./nav/BadgerBudsNavbar";
 import BadgerBudsDataContext from "../contexts/BadgerBudsDataContext";
 
 export default function BadgerBuds() {
-
     const [buds, setBuds] = useState([]);
 
     useEffect(() => {
-        fetch('https://cs571api.cs.wisc.edu/rest/s25/hw5/buds', {
+        fetch("https://cs571.org/rest/s25/hw5/buds", {
             headers: {
-                "X-CS571-ID": CS571.getBadgerId()
-            }
+                "X-CS571-ID": CS571.getBadgerId(),
+            },
         })
-            .then(res => res.json())
-            .then(cats => {
-                setBuds(cats)
-            })
+            .then((res) => res.json())
+            .then((cats) => {
+                setBuds(cats);
+            });
     }, []);
 
-    console.log(buds)
+    console.log(buds);
 
-    return <div>
-        <BadgerBudsNavbar />
-        <div style={{ margin: "1rem" }}>
-            <BadgerBudsDataContext.Provider value={buds}>
-                <Outlet />
-            </BadgerBudsDataContext.Provider>
+    return (
+        <div>
+            <BadgerBudsNavbar />
+            <div style={{ margin: "1rem" }}>
+                <BadgerBudsDataContext.Provider value={buds}>
+                    <Outlet />
+                </BadgerBudsDataContext.Provider>
+            </div>
         </div>
-    </div>
+    );
 }
