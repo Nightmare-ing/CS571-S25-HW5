e7176c7ccf6c0bc71df9dde318d96013be3289de
Qingsong Hu <kingsanhu@gmail.com>
2026-06-01 17:47:19 +0800

    Finish step 10 of HW5
    Display multiple images of cats with a carousel

--- src/components/BadgerBudsSummary.jsx
+++ src/components/BadgerBudsSummary.jsx
@@ -1,21 +1,47 @@
 import { useContext, useState } from "react";
-import { Button, Card, Image } from "react-bootstrap";
+import { Button, Card, Carousel, Image } from "react-bootstrap";
 import BadgerBudsDataContext from "../contexts/BadgerBudsDataContext";
 
 export default function BadgerBudsSummary(props) {
     const [showMore, setShowMore] = useState(false);
 
     return (
-        <Card>
-            <Card.Img
-                src={
-                    "https://raw.githubusercontent.com/CS571-S25/hw5-api-static-content/main/cats/" +
-                    props.imgIds[0]
-                }
-                alt={"Image for " + props.name}
-                style={{ aspectRatio: "1/1", objectFit: "cover" }}
-                variant="top"
-            />
+        <Card style={{ overflow: "hidden" }}>
+            {showMore ? (
+                <Carousel>
+                    {props.imgIds.map((imgId) => (
+                        <Carousel.Item>
+                            <img
+                                src={
+                                    "https://raw.githubusercontent.com/CS571-S25/hw5-api-static-content/main/cats/" +
+                                    imgId
+                                }
+                                alt={
+                                    "Image for " +
+                                    props.name +
+                                    "with image id " +
+                                    imgId
+                                }
+                                className="d-block w-100"
+                                style={{
+                                    aspectRatio: "1/1",
+                                    objectFit: "cover",
+                                }}
+                            />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            ) : (
+                <Card.Img
+                    src={
+                        "https://raw.githubusercontent.com/CS571-S25/hw5-api-static-content/main/cats/" +
+                        props.imgIds[0]
+                    }
+                    alt={"Image for " + props.name}
+                    style={{ aspectRatio: "1/1", objectFit: "cover" }}
+                    variant="top"
+                />
+            )}
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
                 {showMore && (
